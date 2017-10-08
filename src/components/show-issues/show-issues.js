c4fcf897fab69250865b575e71c83ced11fe6546
ipin4 <pin4vital@gmail.com>
2017-10-08 11:32:42 +0300

    Add show issue tab

---
+++ src/components/show-issues/show-issues.js
@@ -0,0 +1,8 @@
+class ShowIssues extends Polymer.Element {
+  static get is () { return 'show-issues'; }
+  static get properties () {
+    return {};
+  }
+
+}
+window.customElements.define(ShowIssues.is, ShowIssues);
--- src/components/view3/view3.js
+++
@@ -1,8 +0,0 @@
-class ViewThree extends Polymer.Element {
-  static get is () { return 'view-three'; }
-  static get properties () {
-    return {};
-  }
-
-}
-window.customElements.define(ViewThree.is, ViewThree);
