14d067ab43b742522c6b6e59f72df61387c07038
ipin4 <pin4vital@gmail.com>
2017-11-07 00:24:55 +0300

    Add repo-list component

--- src/components/repo-list/repo-list.js
+++ src/components/repo-list/repo-list.js
@@ -3,18 +3,59 @@ class RepoList extends Polymer.Element {
 
   static get properties () {
     return {
-      userData: {
-        type: Object,
-        value: {
-          login: '',
-          password: ''
-        }
+      url: {
+        type: String,
+        value: ''
+      },
+      repoData: {
+        type: Array,
+        value: []
+      },
+      linterData: {
+        type: Array,
+        value: []
       },
     };
   }
 
   ready () {
     super.ready();
+    this.beginFetchData('data/json/myrepos.json');
+  }
+
+  beginFetchData (url) {
+    this.set('url', url);
+    this.$.getRepoList.generateRequest();
+  }
+
+  getLintersList (repoId, linterId) {
+    return repoId === linterId;
+  }
+
+  fetchIsDone (value) {
+
+  }
+
+  openRepoById (event) {
+
+  }
+
+  _loadingIsDone () {
+    if (this.responseData[0].description) {
+      const repoSelector = this.$.repoSelector;
+      this.responseData.map(item => {
+        this.repoData.push(item);
+        repoSelector.select(item);
+      });
+      this.beginFetchData ('data/json/linterList.json');
+    } else if (this.responseData[0].linterNames) {
+      const linterSelector = this.$.linterSelector;
+      this.responseData.map(item => {
+        this.linterData.push(item);
+        linterSelector.select(item);
+      });
+      this.fetchIsDone('done');
+    }
   }
 
 }
