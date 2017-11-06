43042979c5a825dca1739440ec5c5f059e47551f
ipin4 <pin4vital@gmail.com>
2017-11-07 00:24:01 +0300

    Fix code issues

--- server/www.js
+++ server/www.js
@@ -1,75 +1,72 @@
-var http = require('http');
-var Static = require('node-static');
-var WebSocketServer = new require('ws');
+const http = require('http');
+const Static = require('node-static');
+const WebSocketServer = new require('ws');
 const fs = require('fs');
-var myTimer;
-// подключенные клиенты
-var clients = {};
+let myTimer;
+//connected clients
+const clients = {};
 //var dataBuild;
 
-// WebSocket-сервер на порту 8081
-var webSocketServer = new WebSocketServer.Server({port: 8082});
-webSocketServer.on('connection', function(ws) {
-
-  var id = Math.random();
+// WebSocket on 8082
+const webSocketServer = new WebSocketServer.Server({port: 8082});
+webSocketServer.on('connection', function (ws) {
+  const id = Math.random();
   clients[id] = ws;
-  console.log("New connection " + id);
+  console.log('New connection ' + id);
 
-  ws.on('message', function(message) {
+  ws.on('message', function (message) {
     console.log('Message is received ' + message);
-    var url;
-    if (message=='getJobInfo') {
+    let url;
+    if (message === 'getJobInfo') {
       url = 'data/build.json';
-      openFile(ws, url);
     }
-    if (message=='echoTest') {
-      ws.send('{"answer": "'+ message +'"}');
+    openFile(ws, url);
+    if (message === 'echoTest') {
+      ws.send('{"answer": "' + message + '"}');
     }
   });
 
-  ws.on('close', function(event) {
+  ws.on('close', function (event) {
     if (event.wasClean) {
-      console.log('Clean closed connection.')
+      console.log('Clean closed connection.');
     } else {
-      console.log('Fall connection. Reason: ' + event.reason)
+      console.log('Fall connection. Reason: ' + event.reason);
     }
     console.log('Connection is closed ' + id);
     clearTimeout(myTimer);
     delete clients[id];
   });
 
-  ws.on('error', function(error) {
-    console.log('Error is: ' + error.message)
+  ws.on('error', function (error) {
+    console.log('Error is: ' + error.message);
   });
-
 });
 
-// обычный сервер (статика) на порту 8080
-var fileServer = new Static.Server('.');
+// static server on 8085
+const fileServer = new Static.Server('.');
 http.createServer(function (req, res) {
   fileServer.serve(req, res);
 }).listen(8085);
 
-function sendJson(ws, dataBuild) {
-  var inc = 0;
-  var length = dataBuild.posts.length;
-  myTimer = setInterval(function() {
-    var post = JSON.stringify(dataBuild.posts[inc]);
+function sendJson (ws, dataBuild) {
+  const inc = 0;
+  const length = dataBuild.posts.length;
+  myTimer = setInterval(function () {
+    const post = JSON.stringify(dataBuild.posts[inc]);
     ws.send(post);
-    if (inc==(length-1)) {
+    if (inc === (length - 1)) {
       console.log('request is done');
       stopTimer();
     }
     inc++;
-  },10)
-
+  }, 10);
 }
 
-function stopTimer() {
+function stopTimer () {
   clearTimeout(myTimer);
 }
 
-function openFile(ws, url) {
+function openFile (ws, url) {
   fs.readFile(url, 'utf-8', function (error, data) {
     if (error) {
       return console.log(error);
@@ -79,4 +76,4 @@ function openFile(ws, url) {
   });
 }
 
-console.log("Server is running on 8082");
+console.log('Server is running on 8082');
